44676152c08a111594c76d781677f65f8ced3b21
Niels Theen <werwofl@googlemail.com>
2016-11-23 20:21:04 +0100

    Exception handling

--- src/HttpServer.php
+++ src/HttpServer.php
@@ -35,6 +35,11 @@ class HttpServer extends EventEmitter
             $this,
             'handleConnection'
         ));
+        
+        if ($errorResponse === null) {
+            $errorResponse = new Response(500);
+        }
+        $this->errorResponse = $errorResponse;
     }
 
     /**
@@ -117,9 +122,15 @@ class HttpServer extends EventEmitter
     public function handleRequest(ConnectionInterface $connection, Request $request)
     {
         $callback = $this->callback;
-        $response = $callback($request);
-        $connection->write(RingCentral\Psr7\str($response));
-        $connection->end();
+
+        try {
+            $response = $callback($request);
+            $connection->write(RingCentral\Psr7\str($response));
+            $connection->end();
+        } catch (\Exception $exception) {
+            $connection->write(RingCentral\Psr7\str($this->errorResponse));
+            $connection->end();
+        }
     }
 
     /**
