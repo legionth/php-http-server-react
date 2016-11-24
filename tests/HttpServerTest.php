00cc222b80f0b2d2b348bad192d9ae627e9b7aba
Niels Theen <werwofl@googlemail.com>
2016-11-24 12:26:50 +0100

    Tests doesn't need stub anymore. Use mock of Connection class instead

--- tests/HttpServerTest.php
+++ tests/HttpServerTest.php
@@ -5,7 +5,6 @@ use Legionth\React\Http\HttpServer;
 use RingCentral\Psr7\Response;
 use RingCentral\Psr7\Request;
 use React\Socket\Connection;
-use Legionth\React\Tests\ConnectionStub;
 
 class HttpServerTest extends TestCase
 {
@@ -13,48 +12,50 @@ class HttpServerTest extends TestCase
     private $loop;
     private $socket;
     private $regularResponse;
+    private $connection;
 
     public function setUp()
     {
         $this->loop = new React\EventLoop\StreamSelectLoop();
         $this->socket = new Socket($this->loop);
-        
+
         $this->regularResponse = new Response();
         $this->httpServer = new HttpServer(
             $this->socket,
             function ($request) {
                 return $this->regularResponse;
             }
-        );
+            );
+
+        $this->connection = $this->getMockBuilder('React\Socket\Connection')->disableOriginalConstructor()->setMethods(array('write', 'end', 'close', 'pause', 'resume', 'isReadable', 'isWritable'))->getMock();
     }
-    
+
     public function testRequestWithoutBody()
     {
         $request = "GET /ip HTTP/1.1\r\nHost: httpbin.org\r\n\r\n";
-        $connection = new ConnectionStub();
-        $this->socket->emit('connection', array($connection));
-        $connection->emit('data', array($request));
-        $this->assertSame("HTTP/1.1 200 OK\r\n\r\n", $connection->getWrittenData());
+
+        $this->socket->emit('connection', array($this->connection));
+        $this->connection->expects($this->once())->method('write')->with($this->equalTo("HTTP/1.1 200 OK\r\n\r\n"));
+        $this->connection->emit('data', array($request));
     }
-    
+
     public function testRequestWithContentLength()
     {
         $request = "GET /ip HTTP/1.1\r\nHost: httpbin.org\r\nContent-Length: 3\r\n\r\n";
         $request .= "bla";
-        
-        $connection = new ConnectionStub();
-        $this->socket->emit('connection', array($connection));
-        $connection->emit('data', array($request));
-        $this->assertSame("HTTP/1.1 200 OK\r\n\r\n", $connection->getWrittenData());
+
+        $this->socket->emit('connection', array($this->connection));
+        $this->connection->expects($this->once())->method('write')->with($this->equalTo("HTTP/1.1 200 OK\r\n\r\n"));
+        $this->connection->emit('data', array($request));
     }
-    
+
     public function testRequestWithChunkedEncodig()
     {
         $request = "GET /ip HTTP/1.1\r\nHost: httpbin.org\r\nTransfer-Encoding: chunked\r\n\r\n";
         $request .= "3\r\nbla\r\n0\r\n\r\n";
-        $connection = new ConnectionStub();
-        $this->socket->emit('connection', array($connection));
-        $connection->emit('data', array($request));
-        $this->assertSame("HTTP/1.1 200 OK\r\n\r\n", $connection->getWrittenData());
+
+        $this->socket->emit('connection', array($this->connection));
+        $this->connection->expects($this->once())->method('write')->with($this->equalTo("HTTP/1.1 200 OK\r\n\r\n"));
+        $this->connection->emit('data', array($request));
     }
 }
